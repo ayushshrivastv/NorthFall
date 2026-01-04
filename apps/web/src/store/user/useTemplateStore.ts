573bae78b02303d0360c2f2aad5df2b4989d5655
ayushshrivastv <ayush.srivastav@icloud.com>
2026-01-04 10:15:00 +0530

    Build contract template storage and retrieval system

---
+++ apps/web/src/store/user/useTemplateStore.ts
@@ -0,0 +1,12 @@
+import { Template } from '@northfall/types';
+import { create } from 'zustand';
+
+interface TemplateState {
+    templates: Template[]; // Global list of available templates
+    setTemplates: (templates: Template[]) => void;
+}
+
+export const useTemplateStore = create<TemplateState>((set) => ({
+    templates: [],
+    setTemplates: (templates) => set({ templates }),
+}));
