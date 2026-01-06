9e00366531a22c44e85da993bdb1484970eb52b8
ayushshrivastv <ayush.srivastav@icloud.com>
2026-01-06 15:15:00 +0530

    Replace application logo text with NorthFall branding

---
+++ apps/web/src/components/tickers/AppLogo.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/src/lib/utils';
+import { LiaServicestack } from 'react-icons/lia';
+import { doto } from '../base/FeatureOne';
+
+interface AppLogoProps {
+    className?: string;
+    size?: number;
+    showLogoText?: boolean;
+}
+
+export default function AppLogo({ className, size = 20, showLogoText = true }: AppLogoProps) {
+    return (
+        <div className={cn('flex items-center gap-x-2', doto.className, className)}>
+            <LiaServicestack size={size} className="text-primary transition-all duration-500" />
+            {showLogoText && <span className="tracking-[0.1rem] font-black">NORTHFALL</span>}
+        </div>
+    );
+}
