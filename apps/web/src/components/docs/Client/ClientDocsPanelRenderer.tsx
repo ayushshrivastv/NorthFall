9d97f0073e7b948c4148053f83a7cb9fbfb91710
ayushshrivastv <ayush.srivastav@icloud.com>
2026-01-09 15:15:00 +0530

    Implement dynamic content renderer for documentation sections

---
+++ apps/web/src/components/docs/Client/ClientDocsPanelRenderer.tsx
@@ -0,0 +1,32 @@
+import { ClientDocsPanel } from '@/src/types/docs-types';
+import ClientGettingStarted from './ClientGettingStarted';
+import ClientOverview from './ClientOverview';
+import ClientE2B from './ClientE2B';
+import ClientDocsExporting from './ClientDocsExporting';
+import ClientDocsDeployments from './ClientDocsDeployments';
+import ClientDocsNorthShell from './ClientDocsNorthShell';
+
+interface ClientDocsPanelRendererProps {
+    clientPanel: ClientDocsPanel;
+}
+
+export default function ClientDocsPanelRenderer({ clientPanel }: ClientDocsPanelRendererProps) {
+    function renderPanels() {
+        switch (clientPanel) {
+            case ClientDocsPanel.OVERVIEW:
+                return <ClientOverview />;
+            case ClientDocsPanel.GETTING_STARTED:
+                return <ClientGettingStarted />;
+            case ClientDocsPanel.SANDBOX:
+                return <ClientE2B />;
+            case ClientDocsPanel.NORTH_SHELL:
+                return <ClientDocsNorthShell />;
+            case ClientDocsPanel.EXPORTING:
+                return <ClientDocsExporting />;
+            case ClientDocsPanel.DEPLOYMENT:
+                return <ClientDocsDeployments />;
+        }
+    }
+
+    return <>{renderPanels()}</>;
+}
